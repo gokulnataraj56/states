9450d33e4c0694f7e9173829a41b10ed8624b7ca
gokulnataraj56 <gokulnataraj56@gmail.com>
2025-03-18 08:19:42 +0530

    useeffect array

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import { useEffect } from 'react'
 function App(){
   const [count ,setCount] =useState(0);
   useEffect(() =>{
-    setCount(10);
-  },[])
-  console.log(count)
+    setCount(100);
+    console.log("use effect");
+    
+  },[count])
+  console.log(count);
   return(
+    <>
     <h1>{count}</h1>
+    <button type="button" onClick={() => setCount((ok) => ok =ok+10 )} >Add</button>
+    </>
   );
 }
 export default App;
